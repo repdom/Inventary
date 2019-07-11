6a46ed2164e63af2357d6a9b5ff96bf6daa7e699
Juan Thomás Ángel Montilla <juan.thomas.angel@gmail.com>
2019-07-11 18:40:15 -0400

    agregando OrdenCompra

---
+++ DaenerysVentaCompra/Models/OrdenCompra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace DaenerysVentaCompra.Models
+{
+    public class OrdenCompra
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("codigoCompra")]
+        public string CodigoCompra { get; set; }
+
+        [BsonElement("codigoSuplidor")]
+        public string CodigoSuplidor { get; set; }
+
+        [BsonElement("fechaOrden")]
+        public DateTime FechaOrden { get; set; }
+
+        [BsonElement("montoTotal")]
+        public double MontoTotal { get; set; }
+
+        [BsonElement("articulos")]
+        public List<Articulo> Articulos { get; set; }
+    }
+}
